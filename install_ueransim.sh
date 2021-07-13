e3983f616f4b5b21236cde1b54855721a39d3f0b
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-13 07:59:01 +0200

    Create install_ueransim.sh

---
+++ install_ueransim.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Requirements
+## VPS with Ubuntu 16.04 or later
+## CMake 3.17 or later
+## gcc 9.0.0 or later
+## g++ 9.0.0 or later
+
+
+sudo apt update
+sudo apt upgrade
+
+#=========================================
+# Install UERANSIM dependencies
+#=========================================
+sudo apt install -y make gcc g++ libsctp-dev lksctp-tools iproute2 
+sudo snap install cmake --classic
+
+cd /usr/src
+git clone https://github.com/aligungr/UERANSIM
+cd UERANSIM
+make
+
+# in terminal 1
+cd build
+./nr-gnb -c ../config/open5gs-gnb.yaml
+
+# in terminal 2
+cd build
+./nr-ne -c ../config/open5gs-ne.yaml
+
+vim config/open5gs-ne.yaml
